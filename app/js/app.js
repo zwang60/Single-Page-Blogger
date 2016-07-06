a14d719b0784435a00085d6e1030001a20572335
wzstevens <zhewang91@gmail.com>
2016-07-05 21:53:41 -0400

    add admin

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 'use strict'
 
-angular.module('spBlogger',['ui.router','spBlogger.posts','spBlogger.controllers','spBlogger.directives','spBlogger.filters','spBlogger.services']);
+angular.module('spBlogger',['ui.router','spBlogger.posts','spBlogger.controllers','spBlogger.directives','spBlogger.filters','spBlogger.services', 'spBlogger.admin']);
 
 angular.module('spBlogger').value('version','V1.0');
---
+++ app/modules/admin/adminModules.js
@@ -0,0 +1,25 @@
+angular.module('spBlogger.admin', ['spBlogger.admin.controllers'])
+.config(['$stateProvider',
+	function($stateProvider){
+		$stateProvider.state('admin', {
+			url: '/admin',
+			abstract: true,
+			controller: 'AdminController',
+			templateUrl:'modules/admin/views/admin-home.html'
+		})
+		.state('admin.postNew', {
+			url: '/posts/new',
+			controller: 'PostCreationController',
+			templateUrl: 'modules/admin/views/admin-new-post.html'
+		})
+		.state('admin.postUpdate', {
+			url: 'posts/:id/edit',
+			controller: 'PostUpdateController',
+			templateUrl: 'modules/admin/views/admin-update-post.html'
+		})
+		.state('admin.postViewAll', {
+			url: '',
+			controller: 'PostListController',
+			templateUrl: 'modules/admin/views/admin-all-posts.html'
+		})
+	}])
